5592f63029215aa7548f8c0bd49dab491e95a7f9
Daniel-Magar <daniel.t@ekoinfomatics.com>
2022-05-11 18:21:28 +0530

    aboutus responsive

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -4,7 +4,73 @@ const Aboutus = () => {
   return (
     <>
       <section id="about">
-        <div className="about">
+        <div className="abt-main">
+          <div className="about-section">
+            <div className="about-abt" style={{ width: "30%" }}>
+              {/* <p className="abt-text"> About</p> */}
+              <p className="text">
+                <b className="rsv-global">RSV</b>
+                <b className="global">Global</b>
+              </p>
+            </div>
+            <div className="about-abt" style={{ width: "70%" }}>
+              <p className="text">
+                RSV Global, with the leadership experience of 25+ years in the
+                fields of staffing, Recruitment, entire gamut of HR, Operation
+                and Customer Relationship, is geared to be a partner in the
+                areas of:
+              </p>
+            </div>
+          </div>
+          <div className="abt-subcontent">
+            <div className="abt-subcontent-content">
+              <div className="d">
+                <div className="in1">
+                  <i
+                    class="bx bxs-briefcase-alt-2"
+                    style={{ fontSize: "50px", color: "#2800d1" }}
+                  ></i>
+                </div>
+                <div className="in2">Recruitment</div>
+              </div>
+              <div className="d">
+                <div className="in1">
+                  <img src="./staff.png" alt="" width="50px" />
+                </div>
+                <div className="in2">Temp Staffing</div>
+              </div>
+              <div className="d">
+                <div className="in1">
+                  <img src="./balance.png" alt="" width="50px" />
+                </div>
+                <div className="in2">HR Statutory Compliance</div>
+              </div>
+              <div className="d">
+                <div className="in1">
+                  <img src="./payroll.png" alt="" width="50px" />
+                </div>
+                <div className="in2">Payroll Management</div>
+              </div>
+            </div>
+          </div>
+          <div className="ptext">
+            <p className="texttext">
+              Each of the above by nature are heavy in time and cost.
+            </p>
+            <br />
+            <p className="texttext">
+              By undertaking these processess for you, we provide you the
+              opportunity to grow lean, focusing and growing on your core buiess
+              objectives.
+            </p>
+          </div>
+          <div class="flex-container">
+            <div class="flex-item-left">
+              <img src="./group.png" alt="about" className="responsiveimg" />
+            </div>
+          </div>
+        </div>
+        {/* <div className="about">
           <div className="about-item-left">
             <div className="about-abt">
               <p className="section-intro"> About us</p>
@@ -41,7 +107,7 @@ const Aboutus = () => {
           <div className="flex-item fitem">HR Statutory Compliance</div>
           <div className="flex-item">
             {" "}
-            <img src="./payroll.png" alt="" />
+            <img src="./payroll.png" alt="" style={{ width: "50px" }} />
           </div>
           <div className="flex-item fitem">Payroll Management</div>
         </div>
@@ -64,7 +130,7 @@ const Aboutus = () => {
           <div class="flex-item-left">
             <img src="./group.png" alt="" className="img" />
           </div>
-        </div>
+        </div> */}
       </section>
     </>
   );
